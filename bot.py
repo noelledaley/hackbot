1539b70123ec80ec87ba379e361ea69fb96196b1
Noelle Daley <adriannenoelle@gmail.com>
2015-09-11 21:31:47 -0700

    testing api auth

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from slackclient import SlackClient
 import os
 
-token = os.environ['SLACK_TOKEN']
+token = os.environ['SLACK_AUTH']
+sc = SlackClient(token)
+
+print sc.api_call("auth.test")
+print sc.api_call("channels.list")
