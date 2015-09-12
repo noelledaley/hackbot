5e190e3b2a628a96cd7d39536bd3108c62f1b2df
Noelle Daley <adriannenoelle@gmail.com>
2015-09-12 16:02:02 -0700

    building hackbot class

--- bot.py
+++ bot.py
@@ -2,12 +2,33 @@ from slackclient import SlackClient
 import os
 import time
 
-bot = os.environ['SLACK_TOKEN']
-sc = SlackClient(bot)
+class Hackbot(object):
 
-print sc.api_call("auth.test")
+    def __init__(self, token):
+        self.token = token
+        self.client = None
 
-sc.rtm_connect()
-while True:
-    print sc.rtm_read()
-    time.sleep(1)
+    def connect(self):
+        self.client = SlackClient(self.token)
+        self.client.rtm_connect()
+
+    def read(self):
+        while True:
+            for response in self.client.rtm_read():
+                print reponse
+            time.sleep(1)
+
+# sc.rtm_connect()
+# while True:
+#     print sc.rtm_read()
+#     if sc.rtm_read():
+#         if sc.rtm_read()[0].get('text') and sc.rtm_read()[0].get('type') == 'message':
+#             channel = sc.rtm_read()[0].get('channel')
+#             sc.rtm_send_message(channel, 'Hello!')
+
+    # [{u'text': u'are you listening?', u'ts': u'1442096766.000004', u'user': u'U076UD6J1', u'team': u'T06TYHMD4', u'type': u'message', u'channel': u'D0AJ3031V'}]
+
+if __name__ == '__main__':
+    token = os.environ['SLACK_TOKEN']
+    balloonicorn = Hackbot(token)
+    balloonicorn.connect()
