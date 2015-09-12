be4967ff1703564813baa741aa47514faca4f81e
Noelle Daley <adriannenoelle@gmail.com>
2015-09-12 16:32:36 -0700

    adding docstrings

--- bot.py
+++ bot.py
@@ -1,43 +1,49 @@
 from slackclient import SlackClient
 import os
 import time
+import random
 
 class Hackbot(object):
+    """
+    An instance of Hackbot, the Hackbright XI Slack bot. Also known as Balloonicorn.
+    """
 
     def __init__(self, token):
         self.token = token
         self.client = None
 
     def connect(self):
+        """Set up SlackClient and connect to Real Time Messaging API."""
         self.client = SlackClient(self.token)
         self.client.rtm_connect()
 
     def read(self):
+        """
+        Read all events from Real Time Messaging API.
+        Checks if balloonicorn is mentioned.
+        """
         while True:
             for response in self.client.rtm_read():
                 print response
-                
+
                 if response.get('type') == 'message':
                     message = response.get('text')
-                    channel = response.get('channel')
-                    self.reply(message, channel)
+                    if 'balloonicorn' in message:
+                        channel = response.get('channel')
+                        self.reply(message, channel)
                 time.sleep(1)
 
     def reply(self, message, channel):
-        if message == 'hello':
-            self.client.rtm_send_message(channel, 'Hi there!')
+        """
+        When balloonicorn is mentioned, returns a random choice from a list of reponses.
+        """
 
-# sc.rtm_connect()
-# while True:
-#     print sc.rtm_read()
-#     if sc.rtm_read():
-#         if sc.rtm_read()[0].get('text') and sc.rtm_read()[0].get('type') == 'message':
-#             channel = sc.rtm_read()[0].get('channel')
-#             sc.rtm_send_message(channel, 'Hello!')
+        reply = random.choice(['Hi there!', 'Yes?', ';)'])
+        self.client.rtm_send_message(channel, reply)
 
-    # [{u'text': u'are you listening?', u'ts': u'1442096766.000004', u'user': u'U076UD6J1', u'team': u'T06TYHMD4', u'type': u'message', u'channel': u'D0AJ3031V'}]
 
 if __name__ == '__main__':
     token = os.environ['SLACK_TOKEN']
     balloonicorn = Hackbot(token)
     balloonicorn.connect()
+    balloonicorn.read()
